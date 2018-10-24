350873141197e2ca14644bf864948b90347fb40b
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2018-10-24 14:26:10 -0400

    Upload New File

---
+++ structs.h
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Proj2
+//
+//  Created by Eduardo Rubio on 10/11/18.
+//  Struct declaration
+//
+
+#include <stdio.h>
+
+struct Point{
+    
+    double x;
+    double y;
+    
+};
+
+struct Triangle{
+    
+    Point first;
+    Point second;
+    Point third;
+
+};
+
+struct LineSeg{
+  
+    Point first;
+    Point second; 
+    
+};

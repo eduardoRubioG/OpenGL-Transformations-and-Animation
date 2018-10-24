eb04942f3ed198701676103d0a10e6519d4ba013
Eduardo Rubio <eduardo.rubio.jr85@gmail.com>
2018-10-24 14:25:40 -0400

    Upload New File

---
+++ magicTree.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  Proj2
+//
+//  Created by Eduardo Rubio on 10/11/18.
+//  Main driver for the program 
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include "structs.h"
+#include "initializerFunctions.cpp"
+#include "circles.h"
+#include "tree.h"
+#include "tesselate.cpp"
+#include "animation.cpp"
+#include "MouseKeyb.cpp"
+using namespace std;
+
+int main(int argc, char * argv[]) {
+    
+    myglutInit(argc, argv);
+    myInit();
+    createTree();
+    glutMouseFunc(mouse);
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    
+    return 0;
+}
